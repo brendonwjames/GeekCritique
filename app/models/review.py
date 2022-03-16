d106f7baab2f3422f5afb3c84bcc6ad6d92fa6ac
brendonwjames <brendonwjames@gmail.com>
2022-03-16 07:53:05 -0700

    setting up merge

---
+++ app/models/review.py
@@ -0,0 +1,23 @@
+from .db import db
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id'), nullable=False)
+    content = db.Column(db.String(255), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+
+    users = db.relationship('User', back_populates='reviews')
+    games = db.relationship('Game', back_populates='reviews')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'game_id': self.game_id,
+            'content': self.content,
+            'rating': self.rating
+        }
