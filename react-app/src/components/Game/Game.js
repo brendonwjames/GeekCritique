f9dda3146f6a77f42d2605890cbb612ec9645d47
Brendon <brendonwjames@gmail.com>
2022-03-16 11:12:48 -0700

    progress on state issues

--- react-app/src/components/Game/Game.js
+++ react-app/src/components/Game/Game.js
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllGames } from "../../store/games";
@@ -6,14 +6,29 @@ import { getAllGames } from "../../store/games";
 
 const Game = () => {
     const dispatch = useDispatch();
-    const user = useSelector((state) => state.session.user);
+    // const user = useSelector((state) => state.session.user);
+    const allGames = useSelector((state => state.game.games))
+    console.log('GAMES OBJ:', allGames)
+    const games = Object.values(allGames)
+    console.log(games)
 
     useEffect(() => {
         dispatch(getAllGames());
     }, [dispatch])
-    
+
 return (
-    <div>{user.username}</div>
+    <>
+        {/* <div>{user.username}</div> */}
+        {games.map((game) => (
+            <div className='game-div' key={game.id}>
+                {console.log(game)}
+                {game.created_at}
+                {game.description}
+                {game.id}
+                {game.img_src}
+            </div>
+        ))}
+    </>
 )
 
 }
--- react-app/src/store/games.js
+++ react-app/src/store/games.js
@@ -7,7 +7,6 @@ const getGames = (allgames) => ({
 
 export const getAllGames = () => async(dispatch) => {
     const response = await fetch('/games');
-    console.log(response, 'aiming for these games')
 
     if (response.ok) {
         const games = await response.json();
@@ -18,17 +17,6 @@ export const getAllGames = () => async(dispatch) => {
 }
 
 
-// export default function gameReducer (state = {}, action) {
-//     let newState;
-//     switch (action.type) {
-//         case GET_GAMES:
-//             newState = {}
-//             action.games.forEach(game => newState[game.id] = game)
-//             return newState
-//         default:
-//             return state
-//     }
-// }
 
 const initialState = {games:{}}
 
@@ -37,9 +25,9 @@ export default function gameReducer(state = initialState, action) {
     switch (action.type) {
         case GET_GAMES:
             newState = {...state}
-            console.log('action.allgames.games in the reducer:', action.allGames.games)
+            // console.log('action.allgames.games in the reducer:', action.allGames.games)
             action.allGames.games.forEach(game => newState.games[game.id] = game)
-            console.log('THE NEW STATE:', newState)
+            // console.log('THE NEW STATE:', newState)
             return newState
         default:
             return state
