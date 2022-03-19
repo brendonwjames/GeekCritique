870bade3603cb1439ba75770d8850e92b1a1b855
Brendon <brendonwjames@gmail.com>
2022-03-19 10:33:13 -0700

    added basic css for specific post

--- react-app/src/components/Game/GameDetails.js
+++ react-app/src/components/Game/GameDetails.js
@@ -1,12 +1,12 @@
 import React from "react";
 import EditModal from "../Modals/EditModal";
 import DeleteModal from "../Modals/DeleteModal";
+import './GameDetails.css';
 
 const GameDetailsPage = ({ game }) => {
 
     return (
-        <div>
-            GameDetailsPage
+        <div className='game-details-container'>
             <img className='game-details-image' src={game.img_src} alt='Faulty URL'></img>
             <p>{game.name}</p>
             <p>{game.description}</p>
