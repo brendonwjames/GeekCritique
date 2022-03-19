56410f781f54c42d86f92f3f2e635da23931f201
Brendon <brendonwjames@gmail.com>
2022-03-19 10:45:09 -0700

    added conditional render of edit/delete game buttons

--- react-app/src/components/Game/GameDetails.js
+++ react-app/src/components/Game/GameDetails.js
@@ -1,17 +1,20 @@
 import React from "react";
 import EditModal from "../Modals/EditModal";
 import DeleteModal from "../Modals/DeleteModal";
+import { useSelector } from "react-redux";
+
 import './GameDetails.css';
 
 const GameDetailsPage = ({ game }) => {
+    const user = useSelector((state) => state.session.user);
 
     return (
         <div className='game-details-container'>
             <img className='game-details-image' src={game.img_src} alt='Faulty URL'></img>
             <p>{game.name}</p>
             <p>{game.description}</p>
-            <EditModal game={game}/>
-            <DeleteModal game={game}/>
+            {user.id === game.owner_id && <EditModal game={game}/>}
+            {user.id === game.owner_id && <DeleteModal game={game}/>}
         </div>
     );
 };
