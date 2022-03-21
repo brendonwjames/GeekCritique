6b14db41aac3ee79a791d9cdccbdeda400aae561
brendonwjames <brendonwjames@gmail.com>
2022-03-21 09:34:55 -0700

    typo

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -11,7 +11,7 @@ def user_exists(form, field):
     if user:
         raise ValidationError('Email address is already in use.')
 
-
+ 
 def username_exists(form, field):
     # Checking if username is already in use
     username = field.data
